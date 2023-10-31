c111928864dc1766d79ba7f5313cf409c21fe76a
keensyo <k66mdg55@gmail.com>
2023-10-31 23:20:02 +0900

    baseを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://keensyo.github.io'
+  site: 'https://keensyo.github.io',
+  base: '/'
 });
